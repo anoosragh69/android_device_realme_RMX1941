d0cd7b57c7cf579e72d6765bcb2464f9bd46e750
SamarV-121 <samarvispute121@pm.me>
2022-02-25 19:40:09 +0530

    RMX1941: sepolicy: Allow charger to write backlight nodes
     * Fixes screen won't stfu in offline charging
     * Change context of leds nodes since coredomain is prohibited from accessing sysfs_leds
       * https://github.com/LineageOS/android_system_sepolicy/blob/lineage-19.0/private/coredomain.te#L33
    
    Change-Id: Iae8d8449462c799d98e50e12b33e0eeacb080b5b
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS := $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # System Properties
