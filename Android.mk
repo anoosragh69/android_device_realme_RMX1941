PixelOS-Devices/device_realme_RMX1941

RMX1941: Setup blob symlinks
Signed-off-by: P Salik <peersalik16@gmail.com>

Android.mk
@@ -17,6 +17,43 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),RMX1941)
-subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libSoftGatekeeper.so $@
+
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6765.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.oppo6762.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6765.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.oppo6762.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
+
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.mt6765.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.oppo6762.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.mt6765.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.oppo6762.so
+
+$(KEYSTORE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcTeeKeymaster.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KEYSTORE_SYMLINK)
+
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.default.so
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.default.so
+$(KMSETKEY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf kmsetkey.trustonic.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KMSETKEY_SYMLINK)
+
 endif

anoosragh69/android_device_realme_RMX1941

Android.mk
@@ -17,6 +17,46 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),RMX1941)
-subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+  GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+  GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libSoftGatekeeper.so $@
+
+  GATEKEEPER_DEVICE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6765.so
+  GATEKEEPER_DEVICE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.oppo6762.so
+  GATEKEEPER_DEVICE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6765.so
+  GATEKEEPER_DEVICE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.oppo6762.so
+
+$(GATEKEEPER_DEVICE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_DEVICE_SYMLINK)
+
+  KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.mt6765.so
+  KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.oppo6762.so
+  KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.mt6765.so
+  KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.oppo6762.so
+
+$(KEYSTORE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcTeeKeymaster.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KEYSTORE_SYMLINK)
+
+  KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.default.so
+  KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.default.so
+
+$(KMSETKEY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf kmsetkey.trustonic.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KMSETKEY_SYMLINK)
+
 endif