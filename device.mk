20fbf6a84792bad474d62608659ff34c7925e9cd
Anoos Ragh <ok.ano0s6090@gmail.com>
2022-04-15 07:51:02 +0000

    RMX1941: Use GcamGo
    Signed-off-by: Anoos Ragh <ok.ano0s6090@gmail.com>

--- device.mk
+++ device.mk
@@ -189,6 +189,10 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+# GCamGO
+PRODUCT_PACKAGES += \
+    CameraGo
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
